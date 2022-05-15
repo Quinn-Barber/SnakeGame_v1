7bc8839806ccbc232ca2eed992fb0062beb0bb9a
Quinn Barber <qbarber2020@gmail.com>
2022-05-15 16:28:52 -0400

    Add project files.

---
+++ SnakeGame_v1/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace SnakeGame_v1
+{
+    partial class Background
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Score = new System.Windows.Forms.Label();
+            this.Food = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Score
+            // 
+            this.Score.AutoSize = true;
+            this.Score.Location = new System.Drawing.Point(0, 0);
+            this.Score.Name = "Score";
+            this.Score.Size = new System.Drawing.Size(75, 22);
+            this.Score.TabIndex = 0;
+            this.Score.Text = "Score: 0";
+            // 
+            // Food
+            // 
+            this.Food.AutoSize = true;
+            this.Food.BackColor = System.Drawing.Color.Red;
+            this.Food.Location = new System.Drawing.Point(250, 250);
+            this.Food.Name = "Food";
+            this.Food.Size = new System.Drawing.Size(20, 22);
+            this.Food.TabIndex = 2;
+            this.Food.Text = "  ";
+            // 
+            // Background
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 500);
+            this.Controls.Add(this.Food);
+            this.Controls.Add(this.Score);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Background";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Background_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Score;
+        private System.Windows.Forms.Label Food;
+    }
+}
+
